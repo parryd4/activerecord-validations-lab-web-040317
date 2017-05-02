de8a0a760857e6a9d814c2c075d297bd1c1501c4
Dan Parry <parryd424@gmail.com>
2017-05-02 16:14:12 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,33 @@
 class Post < ActiveRecord::Base
+
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 30 }
+  validates :summary, length: { maximum: 30 }
+
+  validates :category, inclusion: { in: %w(Fiction Non-fiction), message: "%{value} is not a valid category"}
+
+  validate :title, :must_be_clickbait
+
+  # validate :category_is_a_choice, :must_be_clickbait
+  #
+  #
+  # def category_is_a_choice
+  #   choice = ["Fiction", "Non-fiction"]
+  #   choice.include?(:category): errors.add()
+  # end
+  #
+  def must_be_clickbait
+    clickbait = ["Won't Believe", "Secret", "Top [0-9]", "Guess"]
+
+    if title.present?
+      clickbait_status = false
+      clickbait.each do |bait|
+        clickbait_status = true if title.include?(bait)
+      end
+
+      errors.add(:title, "must include clickbait ") if !clickbait_status
+    end
+  end
+
 end
